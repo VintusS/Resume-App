5d571304eb8c4e4d1943b2d06d7132cb23d53731
VintusS <dragomir.mindrescu@isa.utm.md>
2023-11-19 16:40:04 +0200

    upd: profile picture, skills and languages

--- Resume App/ContentView.swift
+++ Resume App/ContentView.swift
@@ -8,14 +8,98 @@
 import SwiftUI
 
 struct ContentView: View {
-    
     var body: some View {
-        Image("Background")
-            .resizable()
-            .frame(width: 600, height: 900)
+        ZStack {
+            Image("Background")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                .edgesIgnoringSafeArea(.all)
+            HStack() {
+                VStack(alignment: .leading, spacing: 10) {
+                    ZStack {
+                        Circle()
+                            .frame(width: 80, height: 80)
+                            .foregroundStyle(.blue)
+                        Image("pfp")
+                            .resizable()
+                            .frame(width: 70, height: 70)
+                            .cornerRadius(90)
+                    }
+                    
+                    HStack {
+                        paragraphSymbol()
+                        paragraphTitle(Title: "SKILLS")
+                    }
+                    skills(Skill: "Swift")
+                    skills(Skill: "C++")
+                    skills(Skill: "Python")
+                    skills(Skill: "JavaScript")
+                    skills(Skill: "JSX")
+                    skills(Skill: "React")
+                    Text("")
+                    HStack {
+                        paragraphSymbol()
+                        paragraphTitle(Title: "LANGUAGES")
+                    }
+                    languages(Language: "English")
+                    languages(Language: "Russian")
+                    languages(Language: "Romanian")
+                    Text(" ")
+                    HStack {
+                        paragraphSymbol()
+                        paragraphTitle(Title: "INTERESTS")
+                    }
+                    
+                    
+                }
+                Divider()
+                    .frame(width: 2)
+                    .background(Color.blue)
+                
+                VStack {
+                    Text("lashdfjkalsdkjfaldsjkfaldksj")
+                }
+            }
+        }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+func paragraphSymbol() -> some View {
+    ZStack {
+        Rectangle()
+            .frame(width: 25, height: 25)
+            .cornerRadius(3)
+            .border(.blue, width: 2)
+            .cornerRadius(3)
+            .rotationEffect(.degrees(45))
+            .foregroundStyle(.white)
+        Rectangle()
+            .frame(width: 15, height: 15)
+            .cornerRadius(3)
+            .rotationEffect(.degrees(45))
+            .foregroundStyle(.blue)
+    }
+}
+
+func paragraphTitle(Title: String) -> some View {
+    Text(Title)
+        .foregroundStyle(.blue)
+        .bold()
+}
+
+func skills(Skill: String) -> some View {
+    Text(Skill)
+        .foregroundStyle(.white)
+        .padding(3)
+        .background(.blue)
+        .cornerRadius(5)
+}
+
+func languages(Language: String) -> some View{
+    skills(Skill: Language)
 }
