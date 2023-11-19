e51833992bf2ce6ceb9247abcbc5528ed921bf7b
VintusS <dragomir.mindrescu@isa.utm.md>
2023-11-19 15:01:37 +0200

    Initial Commit

---
+++ Resume App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Resume App
+//
+//  Created by Mindrescu Dragomir on 18.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        Image("Background")
+            .resizable()
+            .frame(width: 600, height: 900, alignment: .topLeading)
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Resume App/Resume_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Resume_AppApp.swift
+//  Resume App
+//
+//  Created by Mindrescu Dragomir on 18.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct Resume_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
